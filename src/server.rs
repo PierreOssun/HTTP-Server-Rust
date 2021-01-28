a4beb93048ba7a3aa4277370f0447b317b10591f
Pierre Ossun <pierre.giraud@mailfence.com>
2021-01-28 09:06:19 +0100

    Return Response instead of Formatter

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 use std::convert::TryFrom;
 use crate::http::{Request, Response, StatusCode};
-use std::io::{Read, Write};
+use std::io::Read;
 
 pub struct Server {
     addr: String
@@ -27,18 +27,23 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request :{}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]){
+                            let response = match Request::try_from(&buffer[..]){
                                 Ok(request) => {
                                     dbg!(request);
-                                    let response = Response::new(
-                                        StatusCode::Ok, 
-                                        Some("<h1> IT WORKS ! </h1>".to_string())
-                                    );
-                                    write!(stream, "{}", response);
+                                        Response::new(
+                                            StatusCode::Ok, 
+                                            Some("<h1> IT WORKS ! </h1>".to_string())
+                                    )
                                 },
-                                Err(e) => println!("Failed to parse a request: {}", e)
-                            }
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
 
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e)
                     }
